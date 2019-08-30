c8893c11f6869e238bead2105157958c20797b06
Gisely Lucas Bernardino <gisely@ciandt.com>
2019-08-30 12:16:38 -0300

    Updated the blog challanges.

--- NVDA-training/lesson-2/blog.js
+++ NVDA-training/lesson-2/blog.js
@@ -5,12 +5,26 @@ document.querySelector('.click-stealer').addEventListener('click', function (e) 
     return false;
 });
 
-function showErrorAlert(isRigthLink) {
-    var errorMessage = document.getElementById('error-message');
-    if(!isRigthLink) {
-        errorMessage.innerHTML = 'Error! Você clicou no link errado';
+function showScreen(isRightLink) {
+    var errorMessage = document.getElementById('error-message'),
+        isRightWord = false;
+    if(!isRightLink) {
+        errorMessage.innerHTML = 'Erro! Você clicou no link errado';
     } else {
-        errorMessage.innerHTML = '';
-        document.getElementById('hideScreen').remove();
+        isRightWord = validateWord();
+        if (isRightLink && isRightWord) {
+            errorMessage.innerHTML = '';
+            document.getElementById('hideScreen').remove();
+        } else {
+            errorMessage.innerHTML = 'Erro! Você inseriu a fruta errada';
+        }
     }
+}
+
+function validateWord() {
+    var message = prompt("What is the fruit?", "");
+    if(message === 'avocado' || message === 'Avocado') {
+        return true;
+    }
+    return false;
 }
